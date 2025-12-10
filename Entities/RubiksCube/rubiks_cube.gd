68730751b810d18367956c33063e7987932d1d3e
Jacob Kulberg <jacobkulberg@gmail.com>
2025-12-10 15:55:18 -0500

    code reformatting

--- Entities/RubiksCube/rubiks_cube.gd
+++ Entities/RubiksCube/rubiks_cube.gd
@@ -6,10 +6,7 @@ extends Node3D
 @export var duration_step := 0.01
 @export var max_moves_queued := 10
 
-var is_rotating := false
-var move_queue: Array[String] = []
-var current_duration := base_duration
-var side_dict := {
+var face_dict := {
 	"X_POS": "X+",
 	"X_NEG": "X-",
 	"Y_POS": "Y+",
@@ -17,6 +14,11 @@ var side_dict := {
 	"Z_POS": "Z+",
 	"Z_NEG": "Z-",
 }
+var turn_helper: RubiksCubeTurnHelper
+
+
+func _ready() -> void:
+	turn_helper = RubiksCubeTurnHelper.new(self)
 
 
 func _physics_process(delta: float) -> void:
@@ -26,13 +28,10 @@ func _physics_process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	var key_event := event as InputEventMouseButton
 	if key_event and key_event.button_index == MOUSE_BUTTON_LEFT and key_event.pressed:
-		var sides: Array[String] = []
-		sides.assign(side_dict.values())
+		var faces: Array[String] = []
+		faces.assign(face_dict.values())
 		# TODO: this is only random temporarily
-		if is_rotating and move_queue.size() < max_moves_queued:
-			move_queue.push_back(sides[randi() % sides.size()])
-		else:
-			rotate_side(sides[randi() % sides.size()])
+		turn_helper.queue_turn(faces[randi() % faces.size()])
 
 		# quickly scale up and down the cube when clicked
 		var scale_tween := create_tween()
@@ -40,103 +39,3 @@ func _input(event: InputEvent) -> void:
 		scale_tween.set_trans(Tween.TRANS_CUBIC)
 		scale_tween.tween_property(self, "scale", Vector3.ONE * 1.125, 0.05) # magic numbers but whatev
 		scale_tween.tween_property(self, "scale", Vector3.ONE, 0.1)
-
-
-func rotate_side(side: String) -> void:
-	# dont rotate while rotating
-	if is_rotating:
-		return
-
-	is_rotating = true
-
-	var target_duration := base_duration - duration_step * move_queue.size()
-	if target_duration < min_duration:
-		target_duration = min_duration
-
-	if target_duration < current_duration:
-		current_duration = current_duration - duration_step
-		if current_duration < target_duration:
-			current_duration = target_duration
-	elif target_duration > current_duration:
-		current_duration = current_duration + duration_step
-		if current_duration > target_duration:
-			current_duration = target_duration
-
-	var duration := current_duration
-
-	# get all pieces on side
-	var pieces: Array[Node3D] = []
-	pieces.assign(get_tree().get_nodes_in_group(side))
-
-	# create rotation helper
-	var rotation_helper := Node3D.new()
-	rotation_helper.name = "RotationHelper"
-	self.add_child(rotation_helper)
-
-	# reparent pieces to rotation helper
-	for piece in pieces:
-		piece.reparent(rotation_helper)
-
-	# rotate 90deg
-	var rotation_amount := deg_to_rad(90)
-	# TODO: this is only random temporarily
-	if randi() % 2 == 0:
-		rotation_amount *= -1
-	var target_rotation := Vector3.ZERO
-
-	match side[0]:
-		"X":
-			target_rotation.x += rotation_amount
-		"Y":
-			target_rotation.y += rotation_amount
-		"Z":
-			target_rotation.z += rotation_amount
-
-	# rotate smoothly
-	var tween := create_tween()
-	tween.set_ease(Tween.EASE_IN_OUT)
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(rotation_helper, "rotation", target_rotation, duration)
-	await tween.finished
-
-	# reparent pieces to rubik's cube
-	for piece in pieces:
-		piece.reparent($Pieces)
-
-	# delete rotation helper
-	rotation_helper.queue_free()
-
-	# reassign groups
-	for piece in pieces:
-		# remove all side groups
-		var side_groups: Array[String] = []
-		side_groups.assign(side_dict.values())
-		for group in side_groups:
-			piece.remove_from_group(group)
-
-		var piece_pos: Vector3 = piece.position
-
-		# to account for floating point precision error
-		var threshold := 0.9
-
-		if piece_pos.x >= threshold:
-			piece.add_to_group("X+")
-		elif piece_pos.x <= -threshold:
-			piece.add_to_group("X-")
-
-		if piece_pos.y >= threshold:
-			piece.add_to_group("Y+")
-		elif piece_pos.y <= -threshold:
-			piece.add_to_group("Y-")
-
-		if piece_pos.z >= threshold:
-			piece.add_to_group("Z+")
-		elif piece_pos.z <= -threshold:
-			piece.add_to_group("Z-")
-
-	is_rotating = false
-	if move_queue.size() > 0:
-		var next_side := move_queue[0]
-		move_queue.remove_at(0)
-
-		rotate_side(next_side)
---
+++ Entities/RubiksCube/rubiks_cube_turn_helper.gd
@@ -0,0 +1,150 @@
+class_name RubiksCubeTurnHelper
+extends RefCounted
+
+var cube: RubiksCube
+var is_turning := false
+var turn_queue: Array[String] = []
+var current_duration: float
+var base_duration: float
+var min_duration: float
+var duration_step: float
+var max_turns_queued: int
+
+
+func _init(rubiks_cube: RubiksCube) -> void:
+	cube = rubiks_cube
+	base_duration = cube.base_duration
+	min_duration = cube.min_duration
+	duration_step = cube.duration_step
+	max_turns_queued = cube.max_moves_queued
+	current_duration = base_duration
+
+
+func queue_turn(face: String) -> void:
+	if is_turning and turn_queue.size() < max_turns_queued:
+		turn_queue.push_back(face)
+	else:
+		make_turn(face)
+
+
+func make_turn(face: String) -> void:
+	if is_turning:
+		return
+
+	is_turning = true
+
+	var duration := _calculate_duration()
+	var pieces := _get_pieces_on_face(face)
+	var turn_helper := _create_turn_helper(pieces)
+	var turn_rotation := _calculate_turn_rotation(face)
+
+	await _animate_turn(turn_helper, turn_rotation, duration)
+
+	_reparent_pieces_to_cube(pieces)
+	turn_helper.queue_free()
+	_reassign_piece_groups(pieces)
+
+	is_turning = false
+	_process_next_turn()
+
+
+func _calculate_duration() -> float:
+	var target_duration := base_duration - duration_step * turn_queue.size()
+	if target_duration < min_duration:
+		target_duration = min_duration
+
+	if target_duration < current_duration:
+		current_duration = current_duration - duration_step
+		if current_duration < target_duration:
+			current_duration = target_duration
+	elif target_duration > current_duration:
+		current_duration = current_duration + duration_step
+		if current_duration > target_duration:
+			current_duration = target_duration
+
+	return current_duration
+
+
+func _get_pieces_on_face(face: String) -> Array[Node3D]:
+	var pieces: Array[Node3D] = []
+	pieces.assign(cube.get_tree().get_nodes_in_group(face))
+	return pieces
+
+
+func _create_turn_helper(pieces: Array[Node3D]) -> Node3D:
+	var turn_helper := Node3D.new()
+	turn_helper.name = "TurnHelper"
+	cube.add_child(turn_helper)
+
+	for piece in pieces:
+		piece.reparent(turn_helper)
+
+	return turn_helper
+
+
+func _calculate_turn_rotation(face: String) -> Vector3:
+	var rotation_amount := deg_to_rad(90)
+	# TODO: this is only random temporarily
+	if randi() % 2 == 0:
+		rotation_amount *= -1
+
+	var turn_rotation := Vector3.ZERO
+	match face[0]:
+		"X":
+			turn_rotation.x += rotation_amount
+		"Y":
+			turn_rotation.y += rotation_amount
+		"Z":
+			turn_rotation.z += rotation_amount
+
+	return turn_rotation
+
+
+func _animate_turn(turn_helper: Node3D, turn_rotation: Vector3, duration: float) -> void:
+	var tween := cube.create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(turn_helper, "rotation", turn_rotation, duration)
+	await tween.finished
+
+
+func _reparent_pieces_to_cube(pieces: Array[Node3D]) -> void:
+	for piece in pieces:
+		piece.reparent(cube.get_node("Pieces"))
+
+
+func _reassign_piece_groups(pieces: Array[Node3D]) -> void:
+	var face_groups: Array[String] = []
+	face_groups.assign(cube.face_dict.values())
+
+	for piece in pieces:
+		# remove all face groups
+		for group in face_groups:
+			piece.remove_from_group(group)
+
+		var piece_pos: Vector3 = piece.position
+
+		# to avoid floating point precision issues
+		var threshold := 0.9
+
+		if piece_pos.x >= threshold:
+			piece.add_to_group("X+")
+		elif piece_pos.x <= -threshold:
+			piece.add_to_group("X-")
+
+		if piece_pos.y >= threshold:
+			piece.add_to_group("Y+")
+		elif piece_pos.y <= -threshold:
+			piece.add_to_group("Y-")
+
+		if piece_pos.z >= threshold:
+			piece.add_to_group("Z+")
+		elif piece_pos.z <= -threshold:
+			piece.add_to_group("Z-")
+
+
+func _process_next_turn() -> void:
+	if turn_queue.size() > 0:
+		var next_face := turn_queue[0]
+		turn_queue.remove_at(0)
+		make_turn(next_face)
